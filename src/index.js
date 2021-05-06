ebd9d69f8d5872de94ddf019453dd9504146756c
lavish7900 <80806932+lavish7900@users.noreply.github.com>
2021-05-06 12:57:49 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// the use of ...fruit or ...name is spread operator
+// const Fruit=["apple","mango","oranges"];
+// const veges=["carrot",...Fruit,"raddish"];
+// console.log(veges);
+
+// const name={fname:"lavish",lname:"upadhaya"};
+// const id={userid:"lavish001",...name};
+// console.log(id)
